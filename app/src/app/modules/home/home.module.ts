014dd7aed54cb9c892c65a34ade62344ebe3a271
Shrouk Mamdouh <shrouk@contactless.io>
2021-01-07 07:22:40 +0200

    Add basic app structure, general style, and set the app environment.

---
+++ app/src/app/modules/home/home.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+import { RepoComponent } from './components/repo/repo.component';
+import { ReposListComponent } from './components/repos-list/repos-list.component';
+
+
+
+@NgModule({
+  declarations: [HomeComponent, ReposListComponent, RepoComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [HomeComponent]
+})
+export class HomeModule { }

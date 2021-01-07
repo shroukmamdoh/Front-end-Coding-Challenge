bb40274dc37077c4a0d350dc350d8ef3344a09ee
Shrouk Mamdouh <shrouk@contactless.io>
2021-01-07 07:44:46 +0200

    create a service for repo and interface for modeling it

---
+++ app/src/app/modules/home/services/home-service.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+@Injectable({
+  providedIn: 'root',
+})
+export class HomeServiceService {
+  constructor() {}
+}

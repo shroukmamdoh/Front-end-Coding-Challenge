3695983daa705f6fa20352c6eebc48517c523216
Shrouk Mamdouh <shrouk@contactless.io>
2021-01-08 07:14:59 +0200

    add unit test for home component

--- app/src/app/modules/home/components/home/home.component.spec.ts
+++ app/src/app/modules/home/components/home/home.component.spec.ts
@@ -1,22 +1,38 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { TEST_REPO, TEST_ROUTES } from 'src/app/shared/shared.test';
+import { RepService } from '../../services/repo.service';
+import { ReposListComponent } from '../repos-list/repos-list.component';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
-
+  const repoService = {
+    getRepos: () =>
+      of(TEST_REPO).pipe(
+        map((e) => {
+          return { repo: e };
+        })
+      ),
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientModule, RouterTestingModule.withRoutes(TEST_ROUTES)],
+      declarations: [HomeComponent, ReposListComponent],
+      providers: [{ provide: RepService, useValue: repoService }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.reposList = TEST_REPO;
   });
 
   it('should create', () => {
